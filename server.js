bc2e6cfaf321b8a3149ab323008de0bc9eb3f406
ros1986 <rosuzaini86@yahoo.com>
2026-01-25 12:30:45 +0800

    Update server.js

--- server.js
+++ server.js
@@ -16,17 +16,43 @@ app.get('/', (req, res) => {
 app.get('/api/students', (req, res) => {
   res.json([
     {
+      StudentID: 1,
       Name: "Ali",
-      Program: "IT",
-      AverageScore: 78,
-      AverageAttendance: 92,
+      Program: "Computer Science",
+      AverageScore: 55,
+      AverageAttendance: 70,
+      RiskLevel: "High Risk"
+    },
+    {
+      StudentID: 2,
+      Name: "Aisyah",
+      Program: "Information Systems",
+      AverageScore: 68,
+      AverageAttendance: 82,
       RiskLevel: "Medium Risk"
     },
     {
+      StudentID: 3,
+      Name: "Daniel",
+      Program: "Data Science",
+      AverageScore: 81,
+      AverageAttendance: 90,
+      RiskLevel: "Low Risk"
+    },
+    {
+      StudentID: 4,
       Name: "Siti",
-      Program: "CS",
-      AverageScore: 85,
-      AverageAttendance: 96,
+      Program: "Software Engineering",
+      AverageScore: 74,
+      AverageAttendance: 76,
+      RiskLevel: "Medium Risk"
+    },
+    {
+      StudentID: 5,
+      Name: "Amir",
+      Program: "Computer Science",
+      AverageScore: 88,
+      AverageAttendance: 95,
       RiskLevel: "Low Risk"
     }
   ]);
